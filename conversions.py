8b576b3d7791ec277e483cb9cd754854149395a2
dwaynewebb86 <dwayne.webb@gmail.com>
2026-03-03 20:45:12 -0500

    commit

--- conversions.py
+++ conversions.py
@@ -14,25 +14,25 @@ def convertCelsiusToFahrenheit(celsius):
 
 def convertFahrenheitToCelsius(fahrenheit):
     """Takes in a float representing a Fahrenheit measurement, and returns that temperature converted into Celsius"""
-    celsius = 0
+    celsius = (fahrenheit - 32) * 5/9
     
     return celsius
 
 def convertFahrenheitToKelvin(fahrenheit):
     """Takes in a float representing a Fahrenheit measurement, and returns that temperature converted into Kelvins"""
-    kelvins = 0
+    kelvins = ((fahrenheit - 32) * 5/9) + 273.15
     
     return kelvins
 
 def convertKelvinToCelsius(kelvins):
     """Takes in a float representing a Kelvin measurement, and returns that temperature converted into Celsius"""
-    celsius = 0
+    celsius = kelvins - 273.15
     
     return celsius
 
 def convertKelvinToFahrenheit(kelvins):
     """Takes in a float representing a Kelvin measurement, and returns that temperature converted into Fahrenheit"""
-    fahrenheit = 0
+    fahrenheit = ((kelvins - 273.15) * 9/5) + 32
     
     return fahrenheit
 
