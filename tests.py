cfeb526735619c499e9ee7ee1fb369caee6bec1f
dwaynewebb86 <dwayne.webb@gmail.com>
2026-03-03 20:05:53 -0500

    commit

--- tests.py
+++ tests.py
@@ -10,7 +10,7 @@ class TestConversions(unittest.TestCase):
 
         print("Test2: 100°C should equal 373.15 K")
         self.assertAlmostEqual(conversions.convertCelsiusToKelvin(100), 373.15, places=2)
-        print("passed: 100°C correctly converted to 3733.15 K")
+        print("passed: 100°C correctly converted to 373.15 K")
 
         print("Test3: 300°C should equal 573.15 K")
         self.assertAlmostEqual(conversions.convertCelsiusToKelvin(300.0), 573.15, places=2)
@@ -18,7 +18,7 @@ class TestConversions(unittest.TestCase):
 
         print("Test4: -40°C should equal 233.15 K")
         self.assertAlmostEqual(conversions.convertCelsiusToKelvin(-40.0), 233.15, places=2)
-        print("passed: -40°C correctly converted to 273.15 K")
+        print("passed: -40°C correctly converted to 233.15 K")
 
         print("Test5: -273.15°C should equal 0 K")
         self.assertAlmostEqual(conversions.convertCelsiusToKelvin(-273.15), 0, places=2)
@@ -45,11 +45,6 @@ class TestConversions(unittest.TestCase):
         print("Test5: 37°C should equal 98.6°F")
         self.assertAlmostEqual(conversions.convertCelsiusToFahrenheit(37), 98.6, places=2)
         print("passed:: 37.0°C correctly converted to 98.6°F")
-
-class TestConversions(unittest.TestCase):
-    def test_convertCelsiusToKelvin(self):
-        # call your function and check the result
-        self.assertAlmostEqual(conversions.convertCelsiusToKelvin(celsius), 300, places=2)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
